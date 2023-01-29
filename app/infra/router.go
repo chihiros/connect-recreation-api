0b90107d55e187c0de7f18fdc643253c74d677b2
chihiros <szrkw2ui2a@gmail.com>
2023-01-29 20:49:53 +0900

    del: RealIPを削除

--- app/infra/router.go
+++ app/infra/router.go
@@ -16,7 +16,6 @@ func NewRouter(conn *ent.Client) *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(RealIP) // middleware.RealIPに"Fly-Client-IP"を追加してます
 
 	controller := controller.NewController(conn)
 	r.Route("/v1", func(r chi.Router) {
@@ -41,37 +40,4 @@ func NewRouter(conn *ent.Client) *chi.Mux {
 	})
 
 	return r
-}
-
-var xForwardedFor = http.CanonicalHeaderKey("X-Forwarded-For")
-var xRealIP = http.CanonicalHeaderKey("X-Real-IP")
-var flyClientIP = http.CanonicalHeaderKey("Fly-Client-IP")
-
-func RealIP(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		if rip := realIP(r); rip != "" {
-			r.RemoteAddr = rip
-		}
-		h.ServeHTTP(w, r)
-	}
-
-	return http.HandlerFunc(fn)
-}
-
-func realIP(r *http.Request) string {
-	var ip string
-
-	if fip := r.Header.Get(flyClientIP); fip != "" {
-		ip = fip
-	} else if xrip := r.Header.Get(xRealIP); xrip != "" {
-		ip = xrip
-	} else if xff := r.Header.Get(xForwardedFor); xff != "" {
-		i := strings.Index(xff, ", ")
-		if i == -1 {
-			i = len(xff)
-		}
-		ip = xff[:i]
-	}
-
-	return ip
 }
