3203f53e9a5dbbc27cae21acead96107c54f2643
chihiros <szrkw2ui2a@gmail.com>
2023-02-24 15:49:11 +0900

    add: Appのログを吐く用のライブラリ追加

---
+++ app/middle/applog/applog.go
@@ -0,0 +1,61 @@
+package applog
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/hashicorp/logutils"
+)
+
+var logLevel string
+
+func SetEnv(env string) {
+	logLevel = "DEBUG"
+	switch env {
+	case "production":
+		logLevel = "WARN"
+	case "staging":
+		logLevel = "DEBUG"
+	default:
+		logLevel = "DEBUG"
+	}
+}
+
+type AppLog struct{}
+
+func NewLog() *AppLog {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel(logLevel),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+
+	return &AppLog{}
+}
+
+func Print(args string) {
+	pc, file, line, _ := runtime.Caller(2)
+	fn := runtime.FuncForPC(pc).Name()
+	msg := fmt.Sprintf("%s %s:%d %s", fn, file, line, args)
+	log.Print(msg)
+}
+
+func (l *AppLog) Debug(args string) {
+	Print("[DEBUG] " + args)
+}
+
+func (l *AppLog) Warn(args string) {
+	Print("[WARN] " + args)
+}
+
+func (l *AppLog) Error(args string) {
+	Print("[ERROR] " + args)
+}
+
+func (l *AppLog) Panic(err error) {
+	Print("[PANIC] " + err.Error())
+	panic(err)
+}
