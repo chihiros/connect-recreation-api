34d04c41a232b55a35a86930937b1ef61b7ef6c8
chihiros <szrkw2ui2a@gmail.com>
2023-03-27 02:07:13 +0900

    fix: リクエストの内容をもとにSlackへ通知する内容を作成する

--- app/elements/contact/interfaces/repository/repository.go
+++ app/elements/contact/interfaces/repository/repository.go
@@ -2,7 +2,7 @@ package repository
 
 import (
 	"app/elements/contact/usecase"
-	"app/middle/applog"
+	"app/middle/webhook"
 	"context"
 	"os"
 )
@@ -15,11 +15,16 @@ func NewContactRepository() *ContactRepository {
 
 func (c *ContactRepository) PostContact(ctx context.Context, req usecase.Request) (usecase.Response, error) {
 	webhook_url := os.Getenv("WEBHOOK_URL")
+	text := "お問い合わせがありました\n\n" +
+		"名前: " + req.Name + "\n" +
+		"メールアドレス: " + req.Email + "\n" +
+		"内容: ```" + req.Content + "```"
 
-	if err != nil {
-		applog.Panic(err)
-	}
+	webhook.PostWebhook(
+		webhook_url,
+		text,
+	)
 
 	res := usecase.Response{Data: "success"}
-	return res, err
+	return res, nil
 }
