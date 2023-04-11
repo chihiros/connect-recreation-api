11cf308918f5512fabe2a118a7143ad93c6b0c4e
chihiros <szrkw2ui2a@gmail.com>
2023-04-11 23:55:01 +0900

    fix: profile用のentityに修正

---
+++ app/elements/profile/entity/profile.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id"`
+	UUID      string    `json:"uiid"`
+	Nickname  string    `json:"nickname"`
+	IconURL   string    `json:"icon_url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
