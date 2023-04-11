afdc4509bbf6a389212849930ee7105320ec9e42
chihiros <szrkw2ui2a@gmail.com>
2023-04-11 23:51:01 +0900

    add: Profile用のentスキーマを追加

---
+++ app/ent/schema/profile.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Profile holds the schema definition for the Profile entity.
+type Profile struct {
+	ent.Schema
+}
+
+// Fields of the Profile.
+func (Profile) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("uid").
+			Immutable().
+			Unique(),
+		field.String("nickname").
+			Unique(),
+		field.String("uuid").
+			Unique(),
+		field.String("icon_url"),
+		field.Time("created_at").
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now),
+	}
+}
