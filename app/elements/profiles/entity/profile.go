1c3fd77ce3225a53a1951a35f05f438995961274
chihiros <szrkw2ui2a@gmail.com>
2023-04-12 00:27:11 +0900

    fix: タイポ修正

--- app/elements/profiles/entity/profile.go
+++ app/elements/profiles/entity/profile.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID        int       `json:"id"`
-	UUID      string    `json:"uiid"`
+	UUID      string    `json:"uuid"`
 	Nickname  string    `json:"nickname"`
 	IconURL   string    `json:"icon_url"`
 	CreatedAt time.Time `json:"created_at"`
