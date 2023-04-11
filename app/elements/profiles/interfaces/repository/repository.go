a4e1a17531078f7c38118dca736bd904e5470a81
chihiros <szrkw2ui2a@gmail.com>
2023-04-12 00:12:46 +0900

    fix: タイポ修正

--- app/elements/profiles/interfaces/repository/repository.go
+++ app/elements/profiles/interfaces/repository/repository.go
@@ -33,7 +33,7 @@ func NewProfileRepository(conn *ent.Client) *ProfileRepository {
 
 func (r *ProfileRepository) PostProfiles(ctx context.Context, req usecase.Request) (usecase.Response, error) {
 	profile, err := r.DBConn.Profile.Create().
-		SetUID(req.UID).
+		SetUUID(req.UUID).
 		SetNickname(req.Nickname).
 		SetIconURL(req.IconURL).
 		SetCreatedAt(time.Now()).
