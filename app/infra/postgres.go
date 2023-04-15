7b024ed7096b59ef7da38c2dab5e643e9f66d3b4
chihiros <szrkw2ui2a@gmail.com>
2023-04-15 16:01:24 +0900

    fix: DB_URLの作成方法を変更

--- app/infra/postgres.go
+++ app/infra/postgres.go
@@ -10,18 +10,13 @@ import (
 )
 
 func NewPostgresConnection() (*ent.Client, error) {
-	var DB_URL string
-	if _, exist := os.LookupEnv("DATABASE_URL"); !exist {
-		DB_URL = fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-			os.Getenv("DB_USER"),
-			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_HOST"),
-			os.Getenv("DB_PORT"),
-			os.Getenv("DB_NAME"),
-		)
-	} else {
-		DB_URL = os.Getenv("DATABASE_URL")
-	}
+	DB_URL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 
 	client, err := ent.Open("postgres", DB_URL)
 	if err != nil {
