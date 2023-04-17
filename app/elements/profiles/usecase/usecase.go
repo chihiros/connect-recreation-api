9b667387accb0669151a120f2ac982af1cd7460e
chihiros <szrkw2ui2a@gmail.com>
2023-04-17 22:47:40 +0900

    add: usecaseにPutProfilesの追加

--- app/elements/profiles/usecase/usecase.go
+++ app/elements/profiles/usecase/usecase.go
@@ -9,12 +9,14 @@ import (
 type ProfileUseCase interface {
 	GetProfiles(context.Context, uuid.UUID) (Response, error)
 	PostProfiles(context.Context, Request) (Response, error)
+	PutProfiles(context.Context, Request) (Response, error)
 	DeleteProfiles(context.Context, uuid.UUID) error
 }
 
 type ProfileRepository interface {
 	GetProfiles(context.Context, uuid.UUID) (Response, error)
 	PostProfiles(context.Context, Request) (Response, error)
+	PutProfiles(context.Context, Request) (Response, error)
 	DeleteProfiles(context.Context, uuid.UUID) error
 }
 
@@ -28,6 +30,10 @@ func (u *ProfileUsecase) GetProfiles(ctx context.Context, uuid uuid.UUID) (Respo
 
 func (u *ProfileUsecase) PostProfiles(ctx context.Context, req Request) (Response, error) {
 	return u.Repository.PostProfiles(ctx, req)
+}
+
+func (u *ProfileUsecase) PutProfiles(ctx context.Context, req Request) (Response, error) {
+	return u.Repository.PutProfiles(ctx, req)
 }
 
 func (u *ProfileUsecase) DeleteProfiles(ctx context.Context, uuid uuid.UUID) error {
