0ff8b78ea4418a1ae935b13d96e9270fa875e3c2
chihiros <szrkw2ui2a@gmail.com>
2023-05-20 23:54:37 +0900

    fix: requestのモデルを修正

--- app/elements/recreations/usecase/request.go
+++ app/elements/recreations/usecase/request.go
@@ -1,8 +1,17 @@
 package usecase
 
+import (
+	"app/elements/recreations/entity"
+
+	"github.com/google/uuid"
+)
+
 type Request struct {
-	UID          string `json:"uid"`
-	Username     string `json:"username"`
-	Mail         string `json:"mail"`
-	PrefectureID int    `json:"prefecture_id"`
+	UserID       uuid.UUID   `json:"user_id"`       // レクリエーションを登録したユーザーのID
+	UUID         uuid.UUID   `json:"uid"`           // レクリエーションのユニークID
+	Genre        entity.JSON `json:"genre"`         // レクリエーションのジャンル
+	Title        string      `json:"title"`         // レクリエーションの名前
+	Content      string      `json:"content"`       // レクリエーションの説明
+	TargetNumber int         `json:"target_number"` // レクリエーションの対象人数
+	RequredTime  int         `json:"requred_time"`  // レクリエーションの所要時間
 }
