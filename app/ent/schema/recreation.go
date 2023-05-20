e5352fac8ee799c24b3f46f76f1f0a8ec98f939d
chihiros <szrkw2ui2a@gmail.com>
2023-05-21 02:16:03 +0900

    fix: gerneのTypeを変更

--- app/ent/schema/recreation.go
+++ app/ent/schema/recreation.go
@@ -1,10 +1,6 @@
 package schema
 
 import (
-	"database/sql/driver"
-	"errors"
-	"fmt"
-	"strings"
 	"time"
 
 	"entgo.io/ent"
@@ -25,7 +21,7 @@ func (Recreation) Fields() []ent.Field {
 		field.UUID("uuid", uuid.UUID{}).
 			Immutable().
 			Unique(),
-		field.JSON("genre", &IntSlice{}), // Use JSON field for 'genre'.
+		field.Ints("genre"),
 		field.String("title"),
 		field.String("content"),
 		field.Int("target_number"),
@@ -37,45 +33,4 @@ func (Recreation) Fields() []ent.Field {
 			Default(time.Now).
 			UpdateDefault(time.Now),
 	}
-}
-
-type IntSlice []int
-
-// Scan implements the sql.Scanner interface.
-func (is *IntSlice) Scan(src interface{}) error {
-	asBytes, ok := src.([]byte)
-	if !ok {
-		return errors.New("Scan source was not []bytes")
-	}
-
-	str := string(asBytes)
-
-	// Remove leading and trailing brackets
-	str = str[1 : len(str)-1]
-
-	// Split on comma
-	parts := strings.Split(str, ",")
-
-	// Convert strings to integers
-	result := make([]int, 0, len(parts))
-	for _, p := range parts {
-		var i int
-		_, err := fmt.Sscanf(p, "%d", &i)
-		if err != nil {
-			return err
-		}
-		result = append(result, i)
-	}
-
-	*is = result
-	return nil
-}
-
-// Value implements the driver.Valuer interface.
-func (is IntSlice) Value() (driver.Value, error) {
-	strs := make([]string, len(is))
-	for i, v := range is {
-		strs[i] = fmt.Sprint(v)
-	}
-	return "{" + strings.Join(strs, ",") + "}", nil
 }
