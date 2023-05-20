8cc0aba94be571a83a43e3f31f63d14feacdba98
chihiros <szrkw2ui2a@gmail.com>
2023-05-20 23:32:25 +0900

    fix: indentの修正

--- app/elements/recreations/entity/recreation.go
+++ app/elements/recreations/entity/recreation.go
@@ -9,13 +9,13 @@ import (
 
 type Recreation struct {
 	ID           int       `json:"id"`
-	UserID       uuid.UUID `json:"user_id"`      // レクリエーションを登録したユーザーのID
-	UUID         uuid.UUID `json:"uid"`          // レクリエーションのユニークID
-	Genre        JSON      `json:"genre"`        // レクリエーションのジャンル
-	Title        string    `json:"title"`        // レクリエーションの名前
-	Content      string    `json:"content"`      // レクリエーションの説明
-	TargetNumber int       `json:"target_number"`       // レクリエーションの対象人数
-	RequredTime  int       `json:"requred_time"` // レクリエーションの所要時間
+	UserID       uuid.UUID `json:"user_id"`       // レクリエーションを登録したユーザーのID
+	UUID         uuid.UUID `json:"uid"`           // レクリエーションのユニークID
+	Genre        JSON      `json:"genre"`         // レクリエーションのジャンル
+	Title        string    `json:"title"`         // レクリエーションの名前
+	Content      string    `json:"content"`       // レクリエーションの説明
+	TargetNumber int       `json:"target_number"` // レクリエーションの対象人数
+	RequredTime  int       `json:"requred_time"`  // レクリエーションの所要時間
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
