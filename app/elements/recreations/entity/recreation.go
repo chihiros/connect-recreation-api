04a4996ca2bbab2c4082fae25c6fd2d578ce6cc6
chihiros <szrkw2ui2a@gmail.com>
2023-05-20 23:10:34 +0900

    fix: entityの修正

--- app/elements/recreations/entity/recreation.go
+++ app/elements/recreations/entity/recreation.go
@@ -1,18 +1,18 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Recreation struct {
-	ID        int       `json:"id"`
-	UUID      string    `json:"uid"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	Target    Target    `json:"target"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
-
-type Target struct {
-	Age    int `json:"age"`
-	Nunmer int `json:"number"`
+	ID           int       `json:"id"`
+	UserID       uuid.UUID `json:"user_id"` // レクリエーションを登録したユーザーのID
+	UUID         uuid.UUID `json:"uid"`     // レクリエーションのユニークID
+	Title        string    `json:"title"`   // レクリエーションの名前
+	Content      string    `json:"content"` // レクリエーションの説明
+	TargetNumber int       `json:"target"`  // レクリエーションの対象人数
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
