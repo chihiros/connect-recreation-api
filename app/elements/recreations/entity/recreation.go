7144586ec1ca5219256adbb781c744618bc434fd
chihiros <szrkw2ui2a@gmail.com>
2023-05-20 23:14:52 +0900

    add: RequredTimeの追加

--- app/elements/recreations/entity/recreation.go
+++ app/elements/recreations/entity/recreation.go
@@ -8,11 +8,12 @@ import (
 
 type Recreation struct {
 	ID           int       `json:"id"`
-	UserID       uuid.UUID `json:"user_id"` // レクリエーションを登録したユーザーのID
-	UUID         uuid.UUID `json:"uid"`     // レクリエーションのユニークID
-	Title        string    `json:"title"`   // レクリエーションの名前
-	Content      string    `json:"content"` // レクリエーションの説明
-	TargetNumber int       `json:"target"`  // レクリエーションの対象人数
+	UserID       uuid.UUID `json:"user_id"`      // レクリエーションを登録したユーザーのID
+	UUID         uuid.UUID `json:"uid"`          // レクリエーションのユニークID
+	Title        string    `json:"title"`        // レクリエーションの名前
+	Content      string    `json:"content"`      // レクリエーションの説明
+	TargetNumber int       `json:"target"`       // レクリエーションの対象人数
+	RequredTime  int       `json:"requred_time"` // レクリエーションの所要時間
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
