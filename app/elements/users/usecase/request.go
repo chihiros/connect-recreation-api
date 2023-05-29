20677da279b4a048a6243083738525e62d0d73e3
chihiros <szrkw2ui2a@gmail.com>
2023-05-30 02:20:49 +0900

    del: テストで作っていたuser apiを削除

--- app/elements/users/usecase/request.go
+++
@@ -1,8 +0,0 @@
-package usecase
-
-type Request struct {
-	UID          string `json:"uid"`
-	Username     string `json:"username"`
-	Mail         string `json:"mail"`
-	PrefectureID int    `json:"prefecture_id"`
-}
