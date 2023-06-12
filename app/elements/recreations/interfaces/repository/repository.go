b405dabccf9cf515d7d308226c1a118eaa36c973
chihiros <szrkw2ui2a@gmail.com>
2023-06-12 12:52:08 +0900

    fix: Recreationの投稿者の情報を追加するように修正

--- app/elements/recreations/interfaces/repository/repository.go
+++ app/elements/recreations/interfaces/repository/repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"app/elements/recreations/usecase"
 	"app/ent"
+	"app/ent/profile"
 	"app/ent/recreation"
 	"app/middle/applog"
 	"context"
@@ -57,15 +58,26 @@ func (r *RecreationRepository) GetRecreations(ctx context.Context, limit, offset
 }
 
 func (r *RecreationRepository) GetRecreationsByID(ctx context.Context, id uuid.UUID) (usecase.Response, error) {
-	user, err := r.DBConn.Recreation.Query().
+	recreation, err := r.DBConn.Recreation.
+		Query().
 		Where(recreation.RecreationIDEQ(id)).
-		All(ctx)
+		Only(ctx)
 
 	if err != nil {
-		panic(err)
+		applog.Panic(err)
 	}
 
-	res := usecase.Response{Data: user[0]}
+	profile, err := r.DBConn.Profile.Query().
+		Where(profile.UUIDEQ(recreation.UserID)).
+		First(ctx)
+
+	if err != nil {
+		applog.Panic(err)
+	}
+
+	recreation.Edges.Profile = profile
+
+	res := usecase.Response{Data: recreation}
 	return res, err
 }
 
