0f79713a7d18bfe369443267c3f541656dab0f27
chihiros <szrkw2ui2a@gmail.com>
2024-03-20 12:12:42 +0900

    ユーザーテーブルに都道府県番号を追加*** ***ユーザーテーブルのカラムにコメントを追加

--- schema/seeds/20240317060216_insert_users_table.sql
+++ schema/seeds/20240317060216_insert_users_table.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 INSERT INTO users
-    (nickname, user_id, icon_url)
+    (nickname, user_id, icon_url, prefecture)
 VALUES
-    ('suzurikawa', 'c2cc015d-5753-4132-86fe-624787ae49df', 'https://avatars.githubusercontent.com/u/1000000001')
-    ,('tanaka', '87cab8ea-2252-4a4d-bc15-3fe6192bbf5d', 'https://avatars.githubusercontent.com/u/1000000002')
+    ('suzurikawa', 'c2cc015d-5753-4132-86fe-624787ae49df', 'https://avatars.githubusercontent.com/u/1000000001', '40')
+    ,('tanaka', '87cab8ea-2252-4a4d-bc15-3fe6192bbf5d', 'https://avatars.githubusercontent.com/u/1000000002', '43')
 ;
 -- +goose StatementEnd
 
